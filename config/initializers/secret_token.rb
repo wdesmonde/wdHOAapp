da4192127a4b2c88db28b82dcb9544fa8ec96273
Wen Zilla <willazen@gmail.com>
2011-05-21 13:18:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdHOAapp::Application.config.secret_token = 'e17c01bddb74e820fef6bdcd491c570d0765c2004bc5279ebe403b8470dc12c441c3639d0bfd9a7e58d7e10e5963cfaf546c1875839025c56e8077ad77a0c5a1'
