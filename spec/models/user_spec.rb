a92ac3ac674fc4d1e82ece5c1e558140bb94f518
Wen Zilla <willazen@gmail.com>
2011-08-18 14:40:20 -0700

    added validates for presence to name and email, added validation for length to name.  tests and validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,29 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @attr = { :name => "Sample Person", :email => "sperson@person.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
 end
