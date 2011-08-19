a77e6cad8c8dcb8a8837c1475f4f7f4664e9bcd6
Wen Zilla <willazen@gmail.com>
2011-08-18 17:21:56 -0700

    added constraints to user model and tests for these; added unique and index on email field in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,21 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  # names have to consist of two or three alphabetical words
+  #   first name and last name must begin with an uppercase letter
+  #   can have a two-part name (e.g., von)
+  #   can have an apostrophe in last name
+  #   Valid examples:  Joe Schmoe, Sean O'Malley, Gustav von Trapp,
+  #     LeDawn Meade
+  name_regex = %r{([A-Z][a-z]{1,20})\s(([A-Z][a-z'][A-Za-z]{1,20})|([a-z]{2,4})\s([A-Z][a-z'][A-Za-z]{1,20}))}
+  # using email validation regex from Rubular
+  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   validates :name, :presence => true,
-    :length => { :maximum => 50 }
-  validates :email, :presence => true
+    #:length => { :maximum => 50 },
+    :format => { :with => name_regex }
+  validates :email, :presence => true,
+    :format => { :with => email_regex },
+    :uniqueness => { :case_sensitive => false }
             
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,11 +30,55 @@ describe User do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-
+  # I removed the length validation, but this long name
+  #   should be invalid according to the format rule
   it "should reject names that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
+  end
+
+  it "should accept valid names" do
+    names = ["Joe Schmoe" "Sean O'Casey" "Gustav von Wundermeir" ]
+    names << "Abcdeabcdeabcdeabcdea ab Abcdefghijabcdefghijab"
+    names << "LeDawn Meade"
+    names.each do |name|
+      valid_name_user = User.new(@attr.merge(:name => name))
+      valid_name_user.should be_valid
+    end
+  end
+
+  it "should reject invalid names" do
+    invalid_names = ["a b" "a" "Abcdefghijklmnopqrstuv Abc"]
+    invalid_names.each do |test|
+      invalid_name_user = User.new(@attr.merge(:name => test))
+      invalid_name_user.should_not be_valid
+    end
+  end    
+
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com UPCASE_ULUSER@line.under.org first.last@nonus.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    invalid_addresses = %w[commain@domain,org no_at_sign.net no.top@leveldomain. withoutdot.no@topleveldomain ]
+    invalid_addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    upcased_email = @attr[:email].upcase
+    # Put a user with a given email address into the database
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
   end
 
 end
