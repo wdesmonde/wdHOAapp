a07c62483e40f811bcfddf8d4f2f2666ffc860ea
Wen Zilla <willazen@gmail.com>
2011-08-26 16:56:11 -0700

    user views and changes to css

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+# by using the symbol ':user', we get Factory Girl to simulate the 
+#    User model.
+
+Factory.define :user do |user|
+  user.name    "Some Won"
+  user.email   "somewon@example.com"
+  user.password "foolishbar"
+  user.password_confirmation  "foolishbar"
+end
+
