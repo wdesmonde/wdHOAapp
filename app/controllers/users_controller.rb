11a6713edc059d9bd176a5c122b155f767dc3d69
Wen Zilla <willazen@gmail.com>
2011-09-13 14:11:31 -0700

    destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
 
   def index
     @title = "All Members"
@@ -52,6 +53,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "#{@user.name} deleted"
+    redirect_to users_path
+  end
+
   private
     def authenticate
       deny_access unless signed_in?
@@ -61,4 +69,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
 end
