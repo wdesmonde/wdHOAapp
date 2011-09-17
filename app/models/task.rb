cbaf83bc3d9631f20ea91fb340e5f9d4380f2024
Wen Zilla <willazen@gmail.com>
2011-09-17 16:27:17 -0700

    tasks model field validations with tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,25 @@ class Task < ActiveRecord::Base
   attr_accessible :content, :priority, :status, :category, :due_date
 
   belongs_to :user
+
+  
+  def due_date_cannot_be_in_past
+    if due_date < Date.today
+      errors.add(:due_date, "Due Date can't be in the past")
+    end
+  end
+
+  validates :content, :presence => true, :length => { :maximum => 500 }
+  validates :user_id, :presence => true
+  validates :priority, :presence => true, 
+    :inclusion => { :in => %w(Emergency High Medium Low) }
+  validates :status, :presence => true,
+    :inclusion => { :in => %w(New Approved "In Progress" Completed Closed) }
+  validates :category, :presence => true,
+    :inclusion => { :in => 
+      %w(Other "Major Maintenance" "Minor Maintenance" Administration Landscaping) }
+  validates :due_date, :presence => true
+  validate  :due_date_cannot_be_in_past
+
   default_scope :order => 'tasks.created_at DESC'
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,8 +7,8 @@ describe Task do
       :content => "sample content for this task",
       :due_date => (Time.now) + (14 * 24 * 60 * 60),
       :status => "New",
-      :priority => "Medium",
-      :catetory => "Minor Maintenance"
+      :priority => "High",
+      :category => "Other"
       }
   end
 
@@ -39,5 +39,66 @@ describe Task do
     it "should have a tasks attribute" do
       @user.should respond_to(:tasks)
     end
+  end
+
+  describe "validations" do
+
+    it "should require a user id" do
+      Task.new(@attr).should_not be_valid
+    end
+
+    it "should require nonblank content" do
+      @user.tasks.build(@attr.merge(:content => "   ")).should_not be_valid
+    end
+
+    it "should reject overly-long content" do
+      @user.tasks.build(@attr.merge(:content => "a" * 501)).should_not be_valid
+    end
+
+    it "should accept valid fields" do
+      @user.tasks.build(@attr).should be_valid
+    end
+  
+    it "should only accept valid stati" do
+      stati = %w(New Approved "In Progress" Completed Closed)
+      stati.each do |stati|
+        valid_status_task = @user.tasks.build(@attr.merge(:status => stati))
+        valid_status_task.should be_valid
+      end
+    end
+
+    it "should reject an invalid status" do
+      @user.tasks.build(@attr.merge(:status => "invalid")).should_not be_valid
+    end
+    
+    it "should only accept valid priorities" do
+      priorities = %w(Emergency High Medium Low)
+      priorities.each do |priorities|
+        @user.tasks.build(@attr.merge(:priority => priorities)).should be_valid
+      end
+    end
+
+    it "should reject an invalid priority" do
+      @user.tasks.build(@attr.merge(:priority => "invalid")).should_not be_valid
+    end
+    
+    it "should only accept valid categories" do
+      categories = %w(Administration Other Landscaping "Minor Maintenance" 
+        "Major Maintenance")
+      categories.each do |categories|
+        @user.tasks.build(@attr.merge(:category => categories)).should be_valid
+      end
+    end
+
+    it "should reject an invalid category" do
+      @user.tasks.build(@attr.merge(:category => "invalid")).should_not be_valid
+    end
+    
+    it "should not accept a due date prior to the create date" do
+      yesterday = 24 * 60 * 60
+      past_due_date_task = @user.tasks.build(@attr.merge(:due_date => Time.now-yesterday))
+      past_due_date_task.should_not be_valid
+    end
+
   end
 end
